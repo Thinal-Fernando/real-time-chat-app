43e5893f716a22d3345fad6d32a805050760f1b8
Thinal <r.thinal.fernando@gmail.com>
2025-09-04 11:43:23 +0530

    setup Flask-SocketIO with random guest usernames and basic connect/disconnect events

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 from flask import Flask, render_template, request, session
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
+import random
 
 
 app = Flask(__name__)
 app.secret_key = "@123"
 
 socketio = SocketIO(app, cors_allowed_origins="*")
+active_users = {}
+
+def genarate_guest_name():
+    return f"Guest{random.randint(1000,9999)}"
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    if "username" not in session:
+        session["username"] = genarate_guest_name()
+    return render_template('index.html', username = session["username"])
 
+
+@socketio.event
+def connect():
+    print("A user connected")
+
+
+@socketio.event
+def disconnect():
+    print("A user disconnected")
 
 
 if __name__ == "__main__":
