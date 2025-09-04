0230a1a911c1d7c4126751aeea9cf469f1a9c14f
Thinal <r.thinal.fernando@gmail.com>
2025-09-04 11:05:32 +0530

    Add Flask app setup with SocketIO for real time communication and initial HTML template

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, session
-
+from flask_socketio import SocketIO
 
 
 app = Flask(__name__)
+app.secret_key = "@123"
+
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/")
 def index():
-    return "Hello"
+    return render_template('index.html')
 
 
 
